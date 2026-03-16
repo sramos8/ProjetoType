e7a0adc2f4e87add3fab623e66794c84b4ff14dd
sramos8 <48965740+sramos8@users.noreply.github.com>
2026-03-16 18:08:01 -0300

    feat: adicionar infra de deploy AWS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  root: '.',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://44.223.65.79:3333',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
